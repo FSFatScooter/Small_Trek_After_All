a67477f4931e86dcfa38b00f1f3ce45fb587dfc9
FSFatScooter <FSFatScooter@gmail.com>
2012-04-21 19:01:04 -0500

    Added a .js file for testing/placeholding the entity drawing functions that will be used in their respective constructors.

---
+++ draw.js
@@ -0,0 +1,24 @@
+function drawEnterprise(){
+	ctx.fillStyle = "77F";
+	paint(function(){
+		rect(2,0,3);
+		rect(1,1);
+		rect(5,1);
+		rect(0,2,1,3);
+		rect(3,2,1,3);
+		rect(6,2,1,3);
+		rect(2,3);
+		rect(4,3);
+		rect(1,5);
+		rect(5,5);
+		rect(2,6,3);
+		rect(3,7);
+		rect(0,8,1,3);
+		rect(2,8,1,3);
+		rect(4,8,1,3);
+		rect(6,8,1,3);
+		rect(1,9);
+		rect(5,9);
+		rect(3,11);
+	},200,200,angle,3,3,-3.5,-6);
+}
